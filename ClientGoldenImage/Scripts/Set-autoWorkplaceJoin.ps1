62c419cdeaeb406a7a0a7135ec9ba8f9178e3ba6
Brian Ehlert <Brian.Ehlert@Citrix.com>
2017-07-19 10:02:52 -0700

    set autoworkplacejoin in image

---
+++ ClientGoldenImage/Scripts/Set-autoWorkplaceJoin.ps1
@@ -0,0 +1,7 @@
+<# This disabled autoWorkplaceJoin
+This setting is important to block the golden image from registering with Azure AD
+when the Active Directory domain is integrated with Azure AD.
+This needs to be coupled with the GPO to enable autoWorkplaceJoin after the VMs are provisioned
+to whatever worker OU exists.
+#>
+Set-ItemProperty -Path HKLM:\Software\Policies\Microsoft\Windows\WorkplaceJoin -Name autoWorkplaceJoin -Value 0
