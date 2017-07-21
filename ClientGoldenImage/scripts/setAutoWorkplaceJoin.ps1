7a63feb83cf375b5105c52261339d10f2d52214e
Brian Ehlert <Brian.Ehlert@Citrix.com>
2017-07-21 14:29:25 -0700

    too fast

--- ClientGoldenImage/scripts/setAutoWorkplaceJoin.ps1
+++ ClientGoldenImage/scripts/setAutoWorkplaceJoin.ps1
@@ -4,4 +4,6 @@ when the Active Directory domain is integrated with Azure AD.
 This needs to be coupled with the GPO to enable autoWorkplaceJoin after the VMs are provisioned
 to whatever worker OU exists.
 #>
+start-sleep -Seconds 10
 Set-ItemProperty -Path HKLM:\Software\Policies\Microsoft\Windows\WorkplaceJoin -Name autoWorkplaceJoin -Value 0
+Start-Sleep -Seconds 2
